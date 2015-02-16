33565d0709b97dc80b8e8b0bd8fd32255ec6846b
Muthukumar Srinivasan <muthukumar.srinivasan@gmail.com>
2015-02-15 20:50:18 -0800

    IS-607-Week3
    Week3 Assignment for for IS607

---
+++ many-many.sql
@@ -0,0 +1,3 @@
+﻿select * from customer
+
+select * from "OrderDetails" t1, customer t2, products t3 order by t1.customer_id
---
+++ one-many.sql
@@ -0,0 +1,4 @@
+﻿select * from authors;
+select * from books;
+
+select t1.autho_id, t1.firstname, t1.lastname, t2.book_id, t2.title from authors t1, books t2 where t1.autho_id=t2.author_id;
